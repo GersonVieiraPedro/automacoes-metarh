7829fbd0b6e23a5cb11cc665b0f99dcacbc79957
Gerson Vieira Pedro <80074869+GersonVieiraPedro@users.noreply.github.com>
2026-02-11 16:52:23 -0300

    mudar para 1 min

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import logging
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
+@app.timer_trigger(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     logging.info('Python timer trigger function executed.')
     
