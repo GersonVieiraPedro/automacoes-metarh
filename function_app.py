67a35092f2735b49b9f9264637dc3e78fc1baf9f
Gerson Vieira Pedro <80074869+GersonVieiraPedro@users.noreply.github.com>
2026-02-11 16:46:25 -0300

    teste

--- function_app.py
+++ function_app.py
@@ -4,12 +4,13 @@ import logging
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-@app.timer_trigger(schedule="0 30 9 * * *", arg_name="myTimer", run_on_startup=False,use_monitor=False) 
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
 
     print("Timer trigger function ran at: " + str(myTimer.schedule_status.next))
     
     if myTimer.past_due:
+        print('horario: ' + str(myTimer.schedule_status.next))
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
